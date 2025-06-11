8eaab52eb48164c0cee38b13ea92feed7d437c04
Aleksandar Yakimov <a.yakimov@sap.com>
2025-06-11 21:37:05 +0300

    Fixed authentication errors

--- blog/oauth2.py
+++ blog/oauth2.py
@@ -1,11 +1,9 @@
 from fastapi import Depends, HTTPException, status
 from jose import JWTError, jwt
 from fastapi.security import OAuth2PasswordBearer
-import token
-
+from . import token as token_module 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
-
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
     """
@@ -18,4 +16,4 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         headers={"WWW-Authenticate": "Bearer"},
     )
 
-    return token.verify_token(token, credentials_exception)
+    return token_module.verify_token(token, credentials_exception)  # <-- use correct module
